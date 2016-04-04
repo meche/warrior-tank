88e14ef9fb7a941a48d8b703d4594aed81fe03fc
Leandro Alexandrino <backmelt@gmail.com>
2016-04-04 16:58:40 -0300

    create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict'
+var gulp = require('gulp');
+
+var jadeForest = {
+    appJs: 'app/**/*.js',
+    css: 'assets/stylesheet/**/*.styl',
+    html: 'assets/**/*.jade',
+    img: 'assets/images/**/*',
+    js: 'assets/javascript/**/*.js'
+};
+var eternalBlossoms = {
+
+};
