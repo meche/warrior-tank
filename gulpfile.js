8b9cba2cdbf8535fc14d2fe96013b29ad393c4bf
Leandro Alexandrino <backmelt@gmail.com>
2016-04-04 17:11:07 -0300

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 'use strict'
+
 var gulp = require('gulp');
+var autoprefixer = require( 'autoprefixer-stylus' );
+var browserSync = require( 'browser-sync' );
+var clean = require( 'gulp-clean' );
+var concat = require( 'gulp-concat' );
+var htmlMin = require( 'gulp-htmlmin' );
+var imageMin = require( 'gulp-imagemin' );
+var jade = require( 'gulp-jade' );
+var jsHint = require( 'gulp-jshint' );
+var rename = require( 'gulp-rename' );
+var sourceMaps = require( 'gulp-sourcemaps' );
+var stylus = require ( 'gulp-stylus' );
+var uglify = require( 'gulp-uglify' );
 
 var jadeForest = {
     appJs: 'app/**/*.js',
     css: 'assets/stylesheet/**/*.styl',
-    html: 'assets/**/*.jade',
+    html: 'dev/**/*.jade',
     img: 'assets/images/**/*',
     js: 'assets/javascript/**/*.js'
 };
-var eternalBlossoms = {
 
-};
+var eternalBlossoms = {};
